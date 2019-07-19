b80c99c391547125d807d063a8bc9a2d9808d505
Sérgio Moura <smouracalmon@gmail.com>
2019-07-19 09:27:34 -0300

    Exibindo o filme corretamente

--- filme.php
+++ filme.php
@@ -1,6 +1,13 @@
 <?php
-	print_r($_GET);
-	die();
+	// Incluindo $filmes
+	include('./includes/filmes.php');
+
+	// Incluindo $generos
+	include('./includes/generos.php');
+
+	$id = 1 * $_GET['id'];
+	$f = $filmes[$id];
+
 ?>
 
 <!DOCTYPE html>
@@ -20,11 +27,11 @@
 <body>
 	<nav>
 		<ul>
-			<li><a href="#">Gênero 1</a></li>
-			<li><a href="#">Gênero 2</a></li>
-			<li><a href="#">Gênero 3</a></li>
-			<li><a href="#">Gênero 4</a></li>
-			<li><a href="#">Gênero 5</a></li>
+			<?php
+				foreach($generos as $g){
+					echo('<li><a href="#">'.$g.'</a></li>');
+				}
+			?>
 		</ul>
 		<form>
 			<input type="text" name="trecho"><button type="submit">Buscar</button>
@@ -32,27 +39,27 @@
 	</nav>
 	<main>
 
-		<h2>Turma da Mônica - Laços</h2>
-		<img src="./assets/img/cartazes/cartaz-3.png" alt="Turma da Mônica - Laços" class="cartaz">
+		<h2><?= $f['titulo'] ?></h2>
+		<img src="./assets/img/cartazes/cartaz-<?= $id ?>.png" alt="<?= $f['titulo'] ?>" class="cartaz">
 		<section>
 
 			<div class="sinopse">
 				<h5>Sinopse</h5>
-				<div>Floquinho, o cachorro do Cebolinha (Kevin Vechiatto), desapareceu. O menino desenvolve então um plano infalível para resgatar o cãozinho, mas para isso vai precisar da ajuda de seus fiéis amigos Mônica (Giulia Benite), Magali (Laura Rauseo) e Cascão (Gabriel Moreira). Juntos, eles irão enfrentar grandes desafios e viver grandes aventuras para levar o cão de volta para casa. Classificação Indicativa livre, sem restrições.</div>
+				<div><?= $f['sinopse'] ?></div>
 			</div>
 
 			<div class="censura">
 				<h5>Censura</h5>
-				<div>Livre</div>
+				<div><?= ($f['censura'] == 0) ? ('Livre') : ($f['censura']) ?></div>
 			</div>
 			
 			<div class="critica">
 				<h5>Crítica</h5>
-				<div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, veniam quia. Quam aperiam eius facere provident officiis suscipit optio earum laudantium aut necessitatibus corrupti qui, dolorem, illum veniam doloribus quae?</div>
+				<div><?= $f['critica'] ?></div>
 			</div>
 		</section>
 
-		<iframe src="https://www.youtube.com/embed/A4k3QGPr7oQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>		
+		<?= $f['trailer'] ?>
 	</main>
 </body>
 
